3886e3ed2d1bde46306c9ca16c86d68c4b24217f
hyeji1221 <lhg2615@naver.com>
2021-02-17 18:09:55 +0900

    OpenCV 이미지 전처리 & 값 출력

--- detector.py
+++ detector.py
@@ -1,7 +1,15 @@
 import cv2 # pip install --upgrade opencv-contrib-python
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+import tensorflow as tf
+import numpy as np
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+
 
 model = 'Face_recognition\opencv_face_detector_uint8.pb'
 config = 'Face_recognition\opencv_face_detector.pbtxt'
+mask_model = tf.keras.models.load_model('./mask_detector.h5') # 임시 모델 사용
 
 cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 
@@ -31,18 +39,39 @@ while True:
     for i in range(detect.shape[0]):
         confidence = detect[i, 2]
         if confidence < 0.5:
-            break
+            continue
 
         x1 = int(detect[i, 3] * w)
         y1 = int(detect[i, 4] * h)
         x2 = int(detect[i, 5] * w)
         y2 = int(detect[i, 6] * h)
 
-        cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0)) # 사각형 그리기
-       # frame = frame[y1 - int(h/4):y2 + h + int(h/4), x1 - int(w/4):x2 + w + int(w/4)] # 이미지 크롭
+        face = frame[y1:y2, x1:x2] # 크롭
+        face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
+        face = cv2.resize(face, (28, 28), 3)
+        face = face / 256
+        face = img_to_array(face)
+        face = preprocess_input(face)
+        face = np.expand_dims(face, axis=0)
+        result = mask_model.predict(face).squeeze()
+        print(mask_model.predict(face).squeeze())
+        mask = result[0]
+        withoutMask = result[1]
+        #(mask, withoutMask) = model.predict(face)[0]
 
-        label = 'Face: %4.3f' % confidence
-        cv2.putText(frame, label, (x1, y1 - 1), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 1, cv2.LINE_AA)
+        if (mask > withoutMask):
+            color = (0, 255, 0)
+            label = 'Mask %d%%' % (mask * 100)
+        else:
+            color = (0, 0, 255)
+            label = 'No Mask %d%%' % (withoutMask * 100) 
+
+    cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0)) # 사각형 그리기
+    #frame = frame[y1 - int(h/4):y2 + h + int(h/4), x1 - int(w/4):x2 + w + int(w/4)] # 이미지 크롭
+
+    #label = 'Face: %4.3f' % label
+    label = 'Face: %s' % label
+    cv2.putText(frame, label, (x1, y1 - 1), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 1, cv2.LINE_AA)
 
     cv2.imshow('frame', frame)
 
